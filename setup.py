fe86b07c651fb7510fe4400a6cf34825a30e1787
Mark Stacy <mbstacy@gmail.com>
2017-01-30 10:32:50 -0600

    change repo to new queue. Separate queues

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #import ez_setup
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
-setup(name='imageq',
+setup(name='xmlgenq',
       version='0.0',
       packages= find_packages(),
-      package_data={'imageq':['tasks/templates/*.tmpl','imageq/tasks/templates/*.tmpl']},
+      package_data={'xmlgenq':['tasks/templates/*.tmpl','xmlgenq/tasks/templates/*.tmpl']},
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
           'paramiko==1.16.0',
           'requests==2.9.1',
-          'pillow==3.3.1',
           'jinja2',
       ],
      dependency_links=[
