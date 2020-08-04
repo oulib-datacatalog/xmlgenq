bc070a1f0d5e6baa7319439f906a948df9710ec6
Tyler Pearson <tdpearson@users.noreply.github.com>
2020-08-04 18:08:07 -0500

    upgrade requests

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='xmlgenq',
-      version='0.0',
+      version='0.1',
       packages= find_packages(),
       package_data={'xmlgenq':['tasks/templates/*.tmpl','xmlgenq/tasks/templates/*.tmpl']},
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
-          'requests==2.20.0',
+          'requests==2.24.0',
           'jinja2',
       ],
      dependency_links=[
